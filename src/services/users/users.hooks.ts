076e23459d410372c9c857e64e916ae741716f63
Haim Vanunu <hvanunu@gmail.com>
2022-06-25 20:38:45 -0700

    iframe soltion

--- src/services/users/users.hooks.ts
+++ src/services/users/users.hooks.ts
@@ -17,11 +17,7 @@ export default {
   },
 
   after: {
-    all: [ 
-      // Make sure the password field is never sent to the client
-      // Always must be the last hook
-      protect('password')
-    ],
+    all: [protect('password')],
     find: [],
     get: [],
     create: [],
